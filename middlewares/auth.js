ff1fb6e510c6b0df917f84f6156c98c05018eecf
kglanos <130902847+kglanos@users.noreply.github.com>
2024-02-19 23:21:56 +0100

    Update middlewares/auth.js
    Co-authored-by: Kamil Mrowka <68347303+KamilMr@users.noreply.github.com>

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../services/schemas/userSchema");
+const { User } = require("../services/schemas/userSchema"); // fix import this is default
 require("dotenv").config();
 
 const verifyToken = async (req, res, next) => {
@@ -7,12 +7,13 @@ const verifyToken = async (req, res, next) => {
         const authHeader = req.headers.authorization;
 
         if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "Not authorized" });
+        return res.status(401).json({ message: "Not authorized" }); // change message 
         }
 
         const token = authHeader.split(" ")[1];
 
         const verify = jwt.verify(token, process.env.JWT_SECRET);
+        console.log(verify); // verify.user.id
         const user = await User.findOne({ _id: verify.id });
 
         if (!user || user.token !== token) {
@@ -21,8 +22,10 @@ const verifyToken = async (req, res, next) => {
         req.user = user;
         next();
         } catch (error) {
-        return res.status(401).json({ message: "Not authorized" });
+            console.log(error); // always console log error 
+        return res.status(401).json({ message: "Not authorized" }); // change massage
     }
 };
 
 module.exports = verifyToken;
+
